5a1556479d354a45bd1c0f73326be4ff854587db
vitta.wang <vitta.wang@126.com>
2025-12-19 16:35:41 +0800

    初始化项目，删除compose

---
+++ app/src/main/java/com/sunspot/odl/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.sunspot.odl
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+    }
+}
